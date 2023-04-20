146ae938d7fd03e0a2e52fde5b56ec35ff4203a7
Pablo <salaberry.pablo@gmail.com>
2023-04-20 17:03:27 -0300

    variables

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            sbyte mySbyte = 127;
+            sbyte mySecondSbyte = -128;
+            byte myByte = 255;
+            byte mySecondByte = 0;
+
+            short myShort = -32768;
+            ushort myUShort = 65535;
+
+            int myInt = 2147483647;
+            int mySecondInt = -2147483648;
+        }
+    }
+}
